2244d85bb037326fe2bf25ac27fd3ae6e9463d38
Ayoub Dkhissi <1999dkhissi@gmail.com>
2026-08-02 15:33:18 +0100

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        foreach(var c in s)
+        {
+            switch (c)
+            {
+                case '(':
+                    stack.Push(')');
+                    break;
+                case '[':
+                    stack.Push(']');
+                    break;
+                case '{':
+                    stack.Push('}');
+                    break;
+                default:
+                    if (stack.Count == 0 || stack.Pop() != c)
+                        return false;
+                    break;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
