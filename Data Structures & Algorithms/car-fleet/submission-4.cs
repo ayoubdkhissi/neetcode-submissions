9c44f8ab7428ac3ad116d2b7b8fcad03d61dc922
Ayoub Dkhissi <1999dkhissi@gmail.com>
2026-08-16 14:27:01 +0100

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        var n = position.Length;
+        var arr = new (int p, int s, double arrive)[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = (position[i], speed[i], (target - position[i]) / (double)speed[i]);
+
+        arr = [.. arr.OrderBy(pair => pair.p)];
+
+        var ans = 1;
+        for (int i = n - 1; i > 0; i--)
+        {
+            if (arr[i-1].arrive > arr[i].arrive)
+            {
+                ans++;
+                continue;
+            }
+            arr[i - 1].arrive = arr[i].arrive;
+
+        }
+
+        return ans;
+    }
+}
+
